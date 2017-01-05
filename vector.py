52deb846a62f4000896528a252488fcc8a106672
Simon Cook <simon.cook@salesforce.com>
2017-01-05 12:03:19 +0000

    Added more functionality to vector class

--- vector.py
+++ vector.py
@@ -1,8 +1,10 @@
 import math
-"""Import the math module"""
 
 class Vector(object):
     """This object represents a vector"""
+
+    CANNOT_NORMALISZE_ZERO_VECTOR_MSG = 'Cannot normalize a zero vector'
+
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -48,16 +50,32 @@ class Vector(object):
             return self.times_scalar(1.0/magnitude)
 
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise Exception(self.CANNOT_NORMALISZE_ZERO_VECTOR_MSG)
 
     def dot_product(self, v_vector):
-        """Calculates the dot product of the vector and v_vector"""
-        dot_product = 0.0
+        """Calculates the dot product of this vector and v_vector"""
+        coordinates_multiplied = [x*y for x, y in zip(self.coordinates, v_vector.coordinates)]
+        return sum(coordinates_multiplied)
 
-        return dot_product
+    def angle_of(self, v_vector, in_degrees=False):
+        """Calculates the angle between two vectors"""
+        try:
+            dot_product = self.dot_product(v_vector)
+            magnitude_self = self.magnitude()
+            magnitude_v_vector = v_vector.magnitude()
+            if in_degrees:
+                return math.degrees(math.acos(dot_product/(magnitude_self*magnitude_v_vector)))
+            else:
+                return math.acos(dot_product/(magnitude_self*magnitude_v_vector))
 
-# Tutorial 1
-#
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALISZE_ZERO_VECTOR_MSG:
+                raise Exception("Cannot compute an angle with zero vector")
+            else:
+                raise e
+
+# ADDING, SUBTRACTING AND SCALAR MULTIPLICATION
+
 #v = Vector([8.218,-9.341])
 #w = Vector([-1.129, 2.111])
 #result_vector = v + w
@@ -73,17 +91,35 @@ class Vector(object):
 #print "The resulting scalar multiplication is ", result_vector
 #result_vector = v.times_scalar(scale)
 
-V = Vector([-0.221, 7.437])
-print "The magnitude of", V, "is", V.magnitude()
+# MAGNITUDE AND DIRECTION
 
-V = Vector([8.813, -1.331, -6.247])
-print "The magnitude of", V, "is", V.magnitude()
+#V = Vector([-0.221, 7.437])
+#print "The magnitude of", V, "is", V.magnitude()
 
-V = Vector([5.581, -2.136])
-print "The direction of", V, "is", V.normalized()
+#print "The magnitude of", V, "is", V.magnitude()
+#V = Vector([8.813, -1.331, -6.247])
 
-V = Vector([1.996, 3.108, -4.554])
-print "The normal vector of", V, "is", V.normalized()
+#V = Vector([5.581, -2.136])
+#print "The direction of", V, "is", V.normalized()
 
-V = Vector([0, 0, 0])
-print "The normal vector of", V, "is", V.normalized()
+#V = Vector([1.996, 3.108, -4.554])
+#print "The normal vector of", V, "is", V.normalized()
+
+#V = Vector([0, 0, 0])
+#print "The normal vector of", V, "is", V.normalized()
+
+V = Vector([7.887, 4.138])
+W = Vector([-8.802, 6.776])
+print "The dot product of ", V, "and", W, "is", V.dot_product(W)
+
+V = Vector([-5.955, -4.904, -1.874])
+W = Vector([-4.496, -8.755, 7.103])
+print "The dot product of ", V, "and", W, "is", V.dot_product(W)
+
+V = Vector([3.183, -7.627])
+W = Vector([-2.668, 5.319])
+print "The angle between ", V, "and", W, "is", V.angle_of(W, False)
+
+V = Vector([7.35, 0.221, 5.188])
+W = Vector([2.751, 8.259, 3.985])
+print "The angle between ", V, "and", W, "is", V.angle_of(W, True)
