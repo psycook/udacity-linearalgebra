d3940ba1c157088c67cf9f22ab3f789595af2ff9
Simon Cook <simon.cook@salesforce.com>
2017-01-05 09:33:10 +0000

    updated to fix all pylint errors and warnings

--- vector.py
+++ vector.py
@@ -1,6 +1,8 @@
 import math
+"""Import the math module"""
 
 class Vector(object):
+    """This object represents a vector"""
     def __init__(self, coordinates):
         try:
             if not coordinates:
@@ -18,35 +20,38 @@ class Vector(object):
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
+    def __eq__(self, v_vector):
+        return self.coordinates == v_vector.coordinates
 
-    def __eq__(self, v):
-        return self.coordinates == v.coordinates
-
-    def __add__(self, v):
-        result_coordinates = [x+y for x, y in zip(self.coordinates, v.coordinates)]
+    def __add__(self, v_vector):
+        result_coordinates = [x+y for x, y in zip(self.coordinates, v_vector.coordinates)]
         return Vector(result_coordinates)
 
-    def __sub__(self, v):
-        result_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+    def __sub__(self, v_vector):
+        result_coordinates = [x-y for x, y in zip(self.coordinates, v_vector.coordinates)]
         return Vector(result_coordinates)
 
     def times_scalar(self, scale):
+        """Calculates the scalar multiplier of the vector by scale"""
         result_coordinates = [x * scale for x in self.coordinates]
         return Vector(result_coordinates)
 
     def magnitude(self):
-        coordinates_squared = [x**2 for x in self.coordinates]  
+        """Calculates the magnitude of the vector"""
+        coordinates_squared = [x**2 for x in self.coordinates]
         return math.sqrt(sum(coordinates_squared))
 
     def normalized(self):
+        """Calculcate the normal (direction) of the vector"""
         try:
             magnitude = self.magnitude()
             return self.times_scalar(1.0/magnitude)
-        
-        except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')  
 
-    def dot_product(self, v):
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
+    def dot_product(self, v_vector):
+        """Calculates the dot product of the vector and v_vector"""
         dot_product = 0.0
 
         return dot_product
@@ -68,17 +73,17 @@ class Vector(object):
 #print "The resulting scalar multiplication is ", result_vector
 #result_vector = v.times_scalar(scale)
 
-v = Vector([-0.221,7.437])
-print "The magnitude of", v, "is", v.magnitude()
+V = Vector([-0.221, 7.437])
+print "The magnitude of", V, "is", V.magnitude()
 
-v = Vector([8.813,-1.331,-6.247])
-print "The magnitude of", v, "is", v.magnitude()
+V = Vector([8.813, -1.331, -6.247])
+print "The magnitude of", V, "is", V.magnitude()
 
-v = Vector([5.581, -2.136])
-print "The direction of",v,"is", v.normalized()
+V = Vector([5.581, -2.136])
+print "The direction of", V, "is", V.normalized()
 
-v = Vector([1.996,3.108,-4.554])
-print "The normal vector of",v,"is", v.normalized()
+V = Vector([1.996, 3.108, -4.554])
+print "The normal vector of", V, "is", V.normalized()
 
-v = Vector([0,0,0])
-print "The normal vector of",v,"is", v.normalized()
+V = Vector([0, 0, 0])
+print "The normal vector of", V, "is", V.normalized()
